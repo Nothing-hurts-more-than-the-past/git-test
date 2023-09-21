fd534cd773ea2adb0f2d32306c72c0200f82dda9
cys <cys@qq.com>
2023-09-21 13:28:53 +0800

    first commit

---
+++ src/main/java/com/cys/git_test/GitTestApplication.java
@@ -0,0 +1,15 @@
+package com.cys.git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cys/git_test/Hello.java
@@ -0,0 +1,14 @@
+package com.cys.git_test;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author CYSstart
+ * @create 2023-09-21 13:19
+ */
+@Controller
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
